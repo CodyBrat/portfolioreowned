c9cb986c6aba56305f9195e3bcb199ad6b34eb4a
Priyabrata Singh <priyabratasingh2005@gmail.com>
2025-10-07 19:25:53 +0530

    fixed paths 1

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -3,37 +3,37 @@ const projects = [
   {
     title: "Galactus",
     link: "https://galactus-three.vercel.app/",
-    src: "../../../public/thumbnail/galactus.png"
+    src: "/thumbnail/galactus.png"
   },
   {
     title: "Entrepreneurship Cell Website",
     link: "https://e-cell2-0.vercel.app/",
-    src: "../../../public/thumbnail/ecell.png"
+    src: "/thumbnail/ecell.png"
   },
   {
     title: "Showly - Ticket Booking App",
     link: "https://showlyama.vercel.app/",
-    src: "../../../public/thumbnail/showly.png"
+    src: "/thumbnail/showly.png"
   },
   {
     title: "Doozy - E-commerce App",
     link: "https://doozy-liart.vercel.app/",
-    src: "../../../public/thumbnail/doozy.png"
+    src: "/thumbnail/doozy.png"
   },
   {
     title: "FixTime - Appointment Scheduling App",
     link: "https://fix-time-7.vercel.app/",
-    src: "../../../public/thumbnail/fixtime.png"
+    src: "/thumbnail/fixtime.png"
   },
   {
     title: "Tekron - College TechFest Website",
     link: "https://tekronfest.com",
-    src: "../../../public/thumbnail/tekron.png"
+    src: "/thumbnail/tekron.png"
   },
   {
     title: "NIKE Figma prototype design",
     link: "https://www.figma.com/proto/nbTtlZbuKgh5N7HRetufYi/priyabrata-singh?node-id=48-125&p=f&t=4RcjLWlK7EvONuPJ-1&scaling=scale-down&content-scaling=fixed&page-id=28%3A4",
-    src: "../../../public/thumbnail/nike.png"
+    src: "/thumbnail/nike.png"
   }
 ];
 
